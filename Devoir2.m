04ce62109b223acce976dd95a42104693bedf129
Axo47 <ezzedinehassibkevin@gmail.com>
2025-10-18 20:15:06 -0400

    cooking hard lol

--- Devoir2.m
+++ Devoir2.m
@@ -1,3 +1,71 @@
+    function [coup, vbf, t, x, y, z]= Devoir2 (option,xy0,vb0,wb0)
+
+    if norm(vb0) >= 100
+        error('La norme de vb0 (%.2f m/s) dépasse la limite de 100 m/s.', norm(vb0));
+    end
+
+    C = constantes();
+    q0 = [vb0(1); vb0(2); vb0(3); xy0(1) ; xy0(2); 0];
+    wb0 = wb0(:);
+    q = q0;
+
+    idx = 1
+    t_debut = 0;
+    t_cur = t_debut;
+    t_max = 100;
+    t_intervalle = 0.01;
+
+    max_steps = round(t_max / t_intervalle) + 1;
+    t = zeros(max_steps, 1);
+    x = zeros(max_steps, 1);
+    y = zeros(max_steps, 1);
+    z = zeros(max_steps, 1);
+
+    espilone  =[100; 100; 100; 1e-3; 1e-3; 1e-3];
+
+    params = {C, option, wb0};   
+
+
+    while t_cur < t_max
+        [qs, m, Err] = SEDRK4t0E(q, t_cur,t_cur + t_intervalle, espilone, @derive_q,params);
+        q = qs;
+    
+        t_cur  = t_cur + t_intervalle;
+        idx = idx + 1;
+
+        t(idx)  = t_cur;
+        x(idx)  = q(4);   
+        y(idx)  = q(5); 
+        z(idx)  = q(6);  
+
+        if est_dans_coupe(C, q(4),q(5), q(6))
+            coup = 0;
+            break
+        elseif est_dans_region_boisee(C, q(4), q(5))
+            coup = 2;
+            break
+        elseif q(6) <= 0
+            coup = 1;
+            break;
+        end
+
+
+        if t_cur >= t_max
+            coup = 1; 
+        end
+
+
+    end
+     vbf = q(1:3);
+     t = t(1:idx);
+    x = x(1:idx);
+    y = y(1:idx);
+    z = z(1:idx);
+
+end
+
+
+
 function C = constantes()
     % Constantes physiques
     C.M_B     = 45.9e-3;        % [kg]  masse balle
@@ -38,14 +106,14 @@ function isDansCoupe = est_dans_coupe(C, x, y, z)
     isDansCoupe = (z <= 0) & (distance_centre <= C.R_COUPE);
 end
 
-function Fg = force_gravite(C, M) %Prends la masse
-    Fg = M * C.G_VECTEUR; % [N]
+function Fg = force_gravite(C) %Prends la masse
+    Fg = C.M_B * C.G_VECTEUR; % [N]
 end
 
 function Fv = force_visqueuse(C, V)
     % Frottement visqueux : Fv = - 1/2 * rho * C_V * A * |v| * v
     vnorm = norm(V);
-    Fv = -(1/2) * C.RHO * C_V * A * vnorm * V; % [N]
+    Fv = -(1/2) * C.RHO * C.C_V * C.A * vnorm * V; % [N]
 end
 
 function FM = force_magnus(C, V, wb)
@@ -60,37 +128,101 @@ end
 
 
 function a = acc_gravite_visqueux(C, V)
-    Fg = force_gravite(C.M_B);
-    Fv = force_visqueuse(V, C);
+    Fg = force_gravite(C);
+    Fv = force_visqueuse(C,V);
     a = (Fg + Fv) / C.M_B;
 end
 
-function a = acc_gravite_visqueux_magnus(C,WB,V)
-    Fg = force_gravite(C.M_B);
-    Fv = force_visqueuse(V, C);
-    Fm = force_magnus(V, WB, C);
+function a = acc_gravite_visqueux_magnus(C,V,WB)
+    Fg = force_gravite(C);
+    Fv = force_visqueuse(C,V);
+    Fm = force_magnus(C ,V, WB);
     a = (Fg + Fv + Fm) / C.M_B;
 end
 
 
-function a = accelerationSelonOption(C,option)
+function a = accelerationSelonOption(C,option,V,W)
     switch option
         case 1
-            a = C.G_VECTEUR
+            a = C.G_VECTEUR;
         case 2   % gravité + frottement visqueux
-            a = acc_gravite_visqueux(C,vb0)
+            a = acc_gravite_visqueux(C,V);
         case 3   % gravité + visqueux + Magnus
-            a = acc_gravite_visqueux_magnus(C,wb0,vb0)
+            a = acc_gravite_visqueux_magnus(C,V,W);
         otherwise
             error('option doit être 1, 2 ou 3');
     end
 end
 
-
-% FONCTION DU DEVOIR 
-function [coup vbf t x y z]= Devoir2 (option,xy0,vb0,wb0)
-
-    C = constantes();
-    a = accelerationSelonOption(C, option)
-
+function q_derive = derive_q(q0, ~, params)
+    C      = params{1};
+    option = params{2};
+    wb0    = params{3};
+    V = q0(1:3);                        
+    a = accelerationSelonOption(C, option, V, wb0); 
+    q_derive = [a(1); a(2); a(3); q0(1); q0(2); q0(3)];
 end
+
+
+function [conv, Err]=ErrSol(qs1,qs0,epsilon)
+% Verification si solution convergee
+%   conv      : variable logique pour convergence
+%               Err<epsilon pour chaque elements
+%   Err       : Difference entre qs1 et qs0
+%   qs1       : nouvelle solution
+%   qs0       : ancienne solution
+%   epsilon   : précision pour chaque variable
+nbele=length(qs0);
+Err=(qs1-qs0);
+conv=1;
+for i=1:nbele
+  conv=conv & abs(Err(i)) < epsilon(i);
+end
+end
+
+
+% Solution equations differentielles par methode de RK4
+function qs=SEDRK4t0(q0,t0,Deltat,derive_q,params)
+    % Equation a resoudre : dq/dt=g(q,t)
+    %   qs        : solution [q(to+Deltat)]
+    %   q0        : conditions initiales [q(t0)]
+    %   Deltat    : intervalle de temps
+    %   g         : membre de droite de ED.
+    k1=feval(derive_q,q0,t0,params);
+    k2=feval(derive_q,q0+k1*Deltat/2,t0+Deltat/2,params);
+    k3=feval(derive_q,q0+k2*Deltat/2,t0+Deltat/2,params);
+    k4=feval(derive_q,q0+k3*Deltat,t0+Deltat,params);
+    qs=q0+Deltat*(k1+2*k2+2*k3+k4)/6;
+end
+
+
+function [qs, m, Err]=SEDRK4t0E(q0,t0,tf,epsilon,derive_q, params)
+    %   qs        : solution [q(tf)]
+    %   q0        : conditions initiales [q(t0)]
+    %   t0        : temps initial
+    %   tf        : temps final
+    %   epsilon   : précision pour chaque variable
+    %   g         : membre de droite de ED.
+    m=1;nbi=1;DeltaT=(tf-t0);
+    % Solution avec m=1
+    qs1=SEDRK4t0(q0,t0,DeltaT,derive_q,params);
+    [conv, Err]=ErrSol(qs1,q0,epsilon);
+    qs2=qs1;
+    % Iteration avec m>1
+    while ~conv
+      DeltaT=DeltaT/2;
+      m=m+1;nbi=nbi*2;
+      qs2=q0;t2=t0;
+      for i=1:nbi
+        qs2=SEDRK4t0(qs2,t2,DeltaT,derive_q,params);
+        t2=t2+DeltaT;
+      end
+      [conv, Err]=ErrSol(qs2,qs1,epsilon);
+      qs1=qs2;
+      if m>10
+        break;
+      end
+    end
+    qs=qs2;
+end
+
